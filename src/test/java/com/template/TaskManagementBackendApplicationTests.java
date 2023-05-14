20422c1602d5cf47af397a30d082eb84e9816102
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2023-05-14 11:11:11 +0900

    github actions でテストを実行する

--- src/test/java/com/template/TaskManagementBackendApplicationTests.java
+++ src/test/java/com/template/TaskManagementBackendApplicationTests.java
@@ -1,5 +1,7 @@
 package com.template;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TaskManagementBackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void sampleTest() {
+		String testMessage = "hello";
+		assertEquals("hello", testMessage);
 	}
-
 }
