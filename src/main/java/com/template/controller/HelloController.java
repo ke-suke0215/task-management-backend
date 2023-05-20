09e4ce8cdb477613ed59fc6612038c9cdee6d4bd
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2023-05-20 15:23:18 +0900

    checkstyleの対応

--- src/main/java/com/template/controller/HelloController.java
+++ src/main/java/com/template/controller/HelloController.java
@@ -3,8 +3,17 @@ package com.template.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 表示サンプル用コントローラ.
+ *
+ * @author isodakeisuke
+ */
 @RestController
 public class HelloController {
+
+	/**
+	 * サンプルメソッド.
+	 */
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World!";
